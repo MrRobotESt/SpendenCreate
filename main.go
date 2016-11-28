e9f8c147ae22d014cb96c053b6ad16fd56bc748e
RSturm <robertdersturm@web.de>
2016-11-28 16:23:32 +0100

    Befor Finish -> Check

--- main.go
+++ main.go
@@ -1,95 +1,89 @@
 package main
 
 import (
+	hp "./helper"
 	sp "./spendenCreator"
+	st "./structs"
 	"flag"
 	"fmt"
 	"sort"
 	"strings"
-	hp "./helper"
-	st "./structs"
 )
 
-
-
 /* Control Sequenz */
-var csvConverter = flag.Bool("csvconvert",false,"csv has to be convert in german iso8859-1 Chars" )
-var startCreator = flag.Bool("start",false,"start the pdf creator" )
+var csvConverter = flag.Bool("csvconvert", false, "csv has to be convert in german iso8859-1 Chars")
+var startCreator = flag.Bool("start", false, "start the pdf creator")
 var checkexistence = flag.Bool("existence", false, "check the existence of Person in DepositData")
-
 
 //Struct for sorting DonateData
 type Deposit []st.DonateData
 
 func main() {
 
+
 	flag.Parse()
+	used := func(a *flag.Flag) {
+
+		switch {
+
+		//Only convert the CSV Files into UTF8
+		case a.Name == "csvconvert":
+			hp.CSVtoUTF8("adressen")
+			hp.CSVtoUTF8("buchung")
 
 
-fmt.Println("OK")
+		//Start and create the PDFs
+		case a.Name == "start":
 
-	switch{
+			pC := sp.PageContentBuilder()
+			adressSlice := hp.GetNameAdressDataFromCSV()
+			donateData := hp.GetDonateData()
 
-	case *csvConverter == true:
-		fmt.Println("TEST")
+			for x := range adressSlice {
 
+				donatePersonData := hp.SearchDonateContent(adressSlice[x].Namen, donateData)
+				summeString := hp.GesamtBetrag(donatePersonData)
+				sort.Sort(Deposit(donatePersonData))
+				check := hp.PersonExistsDepositChecker(donatePersonData)
+				if check == true {
+					period := hp.Period(donatePersonData)
+					sp.PageBuilder(pC, adressSlice[x], donatePersonData, summeString, period)
+				} else {
+					//Automatic Check of Person from Adress.csv exist in Buchungs.csv!
+					hp.WriteToFile("NoDeposit", adressSlice[x].Namen)
+				}
 
-
-
-	case *startCreator == true:
-
-
-		hp.CSVtoUTF8("adressen")
-		hp.CSVtoUTF8("buchung")
-		pC := sp.PageContentBuilder()
-
-		adressSlice := hp.GetNameAdressDataFromCSV()
-		donateData := hp.GetDonateData()
-		fmt.Println(donateData)
-		for x := range adressSlice {
-			fmt.Println(adressSlice[x].Namen)
-			donatePersonData := hp.SearchDonateContent(adressSlice[x].Namen, donateData)
-			summeString := hp.GesamtBetrag(donatePersonData)
-			fmt.Println(donatePersonData)
-			sort.Sort(Deposit(donatePersonData))
-
-			fmt.Println(len(donatePersonData))
-			check := hp.PersonExistsDepositChecker(donatePersonData)
-			if check == true {
-				period := hp.Period(donatePersonData)
-				sp.PageBuilder(pC, adressSlice[x], donatePersonData, summeString, period)
-			} else {
-
-				hp.WriteToFile("NoDeposit", adressSlice[x].Namen)
 			}
+
+		//Check the existence of a Person from BUCHUNG.csv in Adress.csv!
+		case a.Name == "existence":
+
+			adressSlice := hp.GetNameAdressDataFromCSV()
+			donateData := hp.GetDonateData()
+			hp.NamensOnlyInDeposit(donateData, adressSlice)
 
 		}
 
-		hp.NamensOnlyInDeposit(donateData, adressSlice)
-
-
-
 	}
-
+	flag.Visit(used)
 }
 
 /* Help Funcitons */
 
-
 //Needed Sort Libraryfunctions for overriding!
 
-func (a Deposit) Len() int           { return len(a) }
-func (a  Deposit) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a  Deposit) Less(i, j int) bool {
-	datumSliceA := strings.Split(a[i].Buchungsdatum,".")
-	datumSliceB := strings.Split(a[j].Buchungsdatum,".")
+func (a Deposit) Len() int      { return len(a) }
+func (a Deposit) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a Deposit) Less(i, j int) bool {
+	datumSliceA := strings.Split(a[i].Buchungsdatum, ".")
+	datumSliceB := strings.Split(a[j].Buchungsdatum, ".")
 	fmt.Println("*****************")
 	fmt.Println(a[i].Buchungsdatum)
 	fmt.Println(a[j].Buchungsdatum)
 
-	if datumSliceA[2] < datumSliceB[2]{
+	if datumSliceA[2] < datumSliceB[2] {
 		return true
 	}
 
-	return datumSliceA[0] <= datumSliceB[0] &&  datumSliceA[1] <= datumSliceB[1]
+	return datumSliceA[0] <= datumSliceB[0] && datumSliceA[1] <= datumSliceB[1]
 }
